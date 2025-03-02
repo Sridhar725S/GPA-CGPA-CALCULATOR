a2b539ac416952cc6a0b895c2429cd0d91a3c086
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-02 14:25:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -56,12 +56,14 @@ app.post('/api/cgpa', (req, res) => {
     res.json({ cgpa: cgpa.toFixed(3) });
 });
 
+
 // Serve Angular frontend
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
+app.use(express.static(path.join(__dirname, 'frontend/dist/frontend')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/dist/frontend/index.html'));
 });
+
 
 // Start Server
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
